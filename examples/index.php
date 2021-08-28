3e6eee9b4f0a791e49768df283e102183227ba02
fusenuk <reload@gmail.com>
2021-08-28 18:40:24 +0100

    remove extra tab

--- examples/index.php
+++ examples/index.php
@@ -9,7 +9,7 @@ $d = dir('.');
 while (false !== ($entry = $d->read())) {
   
    if( $entry != 'index.php' && $entry != '.' && $entry != '..') {
-		$title = str_replace(array('-', '.php'), ' ', $entry);
+	$title = str_replace(array('-', '.php'), ' ', $entry);
         echo "<a href='".$entry."'>".ucwords($title)."</a><br/>\n";
    }
   
